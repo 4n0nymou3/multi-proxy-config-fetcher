a12f767b8c2f6656094d79a6e7fe70995acfc5ea
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-12-17 16:39:18 +0330

    security_filter.py

--- src/security_filter.py
+++ src/security_filter.py
@@ -72,7 +72,6 @@ class SecurityFilter:
             ],
             "inbounds": [
                 {
-                    "listen": "127.0.0.1",
                     "port": 10808,
                     "protocol": "socks",
                     "settings": {
