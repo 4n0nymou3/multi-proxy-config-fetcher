af6e884c5700249d08d164e60a456a4d1d775379
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2026-06-05 11:22:12 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -46,7 +46,7 @@ ENABLED_PROTOCOLS = {
 
 # Maximum age of configurations in days.
 # Configurations older than this will be considered invalid.
-MAX_CONFIG_AGE_DAYS = 3
+MAX_CONFIG_AGE_DAYS = 1
 
 # --- Sing-box Config Tester Settings ---
 
